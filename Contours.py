e1d3022db9fdcb000a36683f9f34fbe4ca36ff44
Alex <alexandrestefanoss@gmail.com>
2022-04-08 11:01:36 +0200

    01/04/22

--- Contours.py
+++ Contours.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 import matplotlib.image as mpimg
 
-img0 = cv2.imread('/users/licence/il09359/Documents/TD_Image/ProjetTI/ProjetTI_traitement/24.jpg', 0) 
+img0 = cv2.imread('DecoupageDonnees/Traitement/24.jpg', 0) 
 img = cv2.medianBlur(img0, 65)
 
 
@@ -18,7 +18,6 @@ plt.subplot(1,3,3),plt.imshow(sobel_8u,cmap = 'gray')
 plt.title('Sobel abs(CV_64F)'), plt.xticks([]), plt.yticks([])
 
 plt.show()
-
 
 imgN = np.zeros(abs_sobel64f.shape)
 for i in range(abs_sobel64f.shape[0]) :
--- testSobel.py
+++ testSobel.py
@@ -2,28 +2,45 @@ import numpy as np
 import cv2
 import matplotlib.image as mplimp
 import matplotlib.pyplot as plt
+import argparse
 
-img = mplimp.imread("DecoupageDonnees/Traitement/24.jpg")
+kernel = np.ones((5,5), np.uint8)
+img0 = cv2.imread('DecoupageDonnees/Traitement/24.jpg', 0) 
+img = cv2.medianBlur(img0, 65)
+img_dilation = cv2.dilate(img, kernel, iterations = 2)
 # plt.figure()
 # plt.imshow(img)
 # plt.show()
 
-laplacian = cv2.Laplacian(img,cv2.CV_64F)
-sobelx = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
-sobely = cv2.Sobel(img,cv2.CV_64F,0,1,ksize=5)
+sobelx64f = cv2.Sobel(img,cv2.CV_64F,1,0,ksize=5)
+abs_sobel64f = np.absolute(sobelx64f)
+sobel_8u = np.uint8(abs_sobel64f)
 
-plt.subplot(2,2,1),plt.imshow(img,cmap = 'gray')
-plt.title('Original'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,2),plt.imshow(laplacian,cmap = 'gray')
-plt.title('Laplacian'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,3),plt.imshow(sobelx,cmap = 'gray')
-plt.title('Sobel X'), plt.xticks([]), plt.yticks([])
-plt.subplot(2,2,4),plt.imshow(sobely,cmap = 'gray')
-plt.title('Sobel Y'), plt.xticks([]), plt.yticks([])
+# plt.subplot(1,3,3),plt.imshow(sobel_8u,cmap = 'gray')
+# plt.title('Sobel abs(CV_64F)'), plt.xticks([]), plt.yticks([])
 
+imgN = np.zeros(abs_sobel64f.shape)
+for i in range(abs_sobel64f.shape[0]) :
+	for j in range(abs_sobel64f.shape[1]) :
+		if abs_sobel64f[i,j] > 120:
+			imgN[i,j] = 255
+
+plt.figure()
+plt.imshow(imgN,cmap = plt.cm.gray)
 plt.show()
 
-# cv2.Sobel(img, cv2.CV_8U, 1, 0, ksize = 3)
+x = 0
+y = 0
+c = 0
+for i in range(imgN.shape[0]) :
+	for j in range(imgN.shape[1]) :
+		if imgN[i,j] == 255:
+			x = x + i
+			y = y + j
+			c = c +1
 
-# cv2.imshow('a', img)
-# cv2.waitKey(0)
+moy_x = x//c
+moy_y = y//c
+# print(moy_x)
+# print(moy_y)
+
