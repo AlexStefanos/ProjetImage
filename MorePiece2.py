ccb66c93ecda9f6dfc430857d85ed4c564636565
misterfacile <pguan19@gmail.com>
2022-05-08 17:58:41 +0200

    modif morepiece2

--- MorePiece2.py
+++ MorePiece2.py
@@ -1,35 +1,70 @@
 import sys
 import cv2 as cv
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mplimp 
 def main(argv):
     
     default_file = 'DecoupageDonnees/Traitement/6.jpg'
     filename = argv[0] if len(argv) > 0 else default_file
     # Loads an image
     src = cv.imread(cv.samples.findFile(filename), cv.IMREAD_COLOR)
+    src1 = cv.cvtColor(src,cv.COLOR_BGR2RGB)
+    plt.figure()
+    plt.imshow(src1)
+    plt.show()
     # Check if image is loaded fine
     if src is None:
         print ('Error opening image!')
         print ('Usage: hough_circle.py [image_name -- default ' + default_file + '] \n')
         return -1
     
-    
+    #imgz = np.zeros(src.shape[0]//25,src.shape[1]//25,dtype = np.uint8)
     gray = cv.cvtColor(src, cv.COLOR_BGR2GRAY)
     
     
-    gray = cv.medianBlur(gray, 15)
     
-    
-    rows = gray.shape[0]
-    circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
+    if src.shape[0]*src.shape[1] >= 12000000 :
+        
+        gray = cv.medianBlur(gray, 17
+                         )
+        rows = gray.shape[0]
+        circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
                                param1=100, param2=30,
-                               minRadius=100, maxRadius=300)
-    
+                               minRadius=100, maxRadius=500                               
+                               
+                               )
+    elif src.shape[0]*src.shape[1] >= 2000000 and src.shape[0]*src.shape[1] < 12000000 :
+        gray = cv.medianBlur(gray, 7
+                         )
+        rows = gray.shape[0]
+        circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
+                               param1=100, param2=30,
+                               minRadius=90, maxRadius=300                               
+                               
+                               )
+    elif src.shape[0]*src.shape[1] < 2000000 :
+        gray = cv.medianBlur(gray, 7
+                         )
+        rows = gray.shape[0]
+        circles = cv.HoughCircles(gray, cv.HOUGH_GRADIENT, 1, rows / 8,
+                               param1=100, param2=30,
+                               minRadius=80, maxRadius=180                               
+                               
+                               )
     
     if circles is not None:
         circles = np.uint16(np.around(circles))
+        imgz = np.zeros((src.shape[0]//25,src.shape[1]//25),dtype =np.uint8)
         for i in circles[0, :]:
             center = (i[0], i[1])
+            print(i[0],i[1])
+            for x in range(imgz.shape[0]) :
+                for y in range(imgz.shape[1]) :
+                    print(src[(i[1]-imgz.shape[0]//2)+x,(i[0]-imgz.shape[1]//2)-y,0])
+
+            print(i[0],i[1])
+            
             # circle center
             cv.circle(src, center, 1, (0, 100, 100), 3)
             # circle outline
